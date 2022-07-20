5af9908ea02abb08f4e093a86a128da87387a049
Jwhyee <kawnsdud@naver.com>
2022-07-20 17:46:48 +0900

    톰캣 버전 9 -> 10으로 변경, HelloServlet 내용 변경

--- src/main/java/com/example/likelionfirstproject/HelloServlet.java
+++ src/main/java/com/example/likelionfirstproject/HelloServlet.java
@@ -1,26 +1,20 @@
 package com.example.likelionfirstproject;
 
 import java.io.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet("/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=utf-8");
 
-    public void init() {
-        message = "Hello World!";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void destroy() {
+        resp.getWriter().append("Hello Servlet");
     }
 }
