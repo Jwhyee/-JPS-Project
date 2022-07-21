2fa819d6335bd80676cbfbb764a283e742cc4f3d
Jwhyee <kawnsdud@naver.com>
2022-07-21 13:42:00 +0900

    Rq 클래스를 통해 들어오는 파라미터 처리

--- src/main/java/com/example/likelionfirstproject/Rq.java
+++ src/main/java/com/example/likelionfirstproject/Rq.java
@@ -4,20 +4,37 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 public class Rq{
-    private HttpServletResponse resp;
-    Rq(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        req.setCharacterEncoding("UTF-8");
+    private final HttpServletRequest req;
+    private final HttpServletResponse resp;
+    Rq(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            req.setCharacterEncoding("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html; charset=utf-8");
+        this.req = req;
+        this.resp = resp;
     }
 
-    public int getIntParam(String command, int num) {
-        return 1;
+    public int getIntParam(String paramName, int defaultValue) {
+        String value = req.getParameter(paramName);
+
+        if (value == null) {
+            return defaultValue;
+        }
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
     }
 
-    public void appendBody(String str) throws IOException {
-        this.resp.getWriter().append(str);
+    public void appendBody(String formatted) throws IOException {
+        resp.getWriter().append(formatted);
     }
 }
