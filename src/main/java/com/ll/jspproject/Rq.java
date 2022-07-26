98ac7948fcae64c8753c750202ddbf13f0ecadfe
Jwhyee <kawnsdud@naver.com>
2022-07-26 14:43:25 +0900

    Rq 기능 추가

--- src/main/java/com/ll/jspproject/Rq.java
+++ src/main/java/com/ll/jspproject/Rq.java
@@ -1,26 +1,38 @@
 package com.ll.jspproject;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
-public class Rq{
-
+public class Rq {
     private final HttpServletRequest req;
     private final HttpServletResponse resp;
 
     public Rq(HttpServletRequest req, HttpServletResponse resp) {
+        this.req = req;
+        this.resp = resp;
+
         try {
             req.setCharacterEncoding("UTF-8");
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html; charset=utf-8");
-        this.req = req;
-        this.resp = resp;
+    }
+
+    public String getParam(String paramName, String defaultValue) {
+        String value = req.getParameter(paramName);
+
+        if (value == null || value.trim().length() == 0) {
+            return defaultValue;
+        }
+
+        return value;
     }
 
     public int getIntParam(String paramName, int defaultValue) {
@@ -29,6 +41,7 @@ public class Rq{
         if (value == null) {
             return defaultValue;
         }
+
         try {
             return Integer.parseInt(value);
         } catch (NumberFormatException e) {
@@ -36,7 +49,35 @@ public class Rq{
         }
     }
 
-    public void appendBody(String formatted) throws IOException {
-        resp.getWriter().append(formatted);
+    public void appendBody(String str) {
+        try {
+            resp.getWriter().append(str);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setAttr(String name, Object value) {
+        req.setAttribute(name, value);
+    }
+
+    public void view(String path) {
+        // gugudan2.jsp 에게 나머지 작업을 토스
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/jsp/" + path + ".jsp");
+        try {
+            requestDispatcher.forward(req, resp);
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getPath() {
+        return req.getRequestURI();
+    }
+
+    public String getMethod() {
+        return req.getMethod();
     }
 }
