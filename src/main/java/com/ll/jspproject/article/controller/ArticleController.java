001399868e1a90de9f9f1f1202244045294fc0ce
Jwhyee <kawnsdud@naver.com>
2022-08-02 10:43:26 +0900

    @Controller, @GetMapping 애노테이션 생성 및 Controller에 연결

--- src/main/java/com/ll/jspproject/article/controller/ArticleController.java
+++ src/main/java/com/ll/jspproject/article/controller/ArticleController.java
@@ -1,4 +1,13 @@
 package com.ll.jspproject.article.controller;
 
+import com.ll.jspproject.annotation.Controller;
+import com.ll.jspproject.annotation.GetMapping;
+
+@Controller
 public class ArticleController {
+
+    @GetMapping("usr/article/list")
+    public void showList() {
+
+    }
 }
